082251fd93ef6102531856dcab39feda4faabc67
kubbixx@web.de <matthias.kasel@lse.thm.de>
2022-08-27 13:12:04 +0200

    Added templates

--- website/views.py
+++ website/views.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint("views",  __name__)
 
 @views.route("/")
 def index():
-    return "HI"
+    return render_template("index.html")
+
+@views.route("/vita")
+def vita():
+    return render_template("base.html")
+
+@views.route("/galerie")
+def galery():
+    return render_template("galery.html", images=[1, 2, 3, 4])
